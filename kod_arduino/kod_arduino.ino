80b8da15f2285e547597e2b447cf884d2fcdf509
Arturo <grabowski.a93@gmail.com>
2019-04-06 11:27:58 +0200

    All sensors in JSON format & code refactoring & disable debug mode

--- kod_arduino/kod_arduino.ino
+++ kod_arduino/kod_arduino.ino
@@ -2,13 +2,14 @@
 // https://drive.google.com/drive/folders/1x29EhA_QyLCDvwt1uYwJfcXbnrRn8K5H?usp=sharing
 
 //Tools -> serialMonitor tam leci serial print
-//kable :
-//czerwony 5 V
-//czarny masa
-//zolty clock
+//wires :
+//red 5 V
+//orange 3,3 V
+//black ground
+//yellow clock
+//green SDA 
 int lap_number = 0;
 #include <Wire.h>
-
 
 //dht22 ******************************************
 #include <dht.h>
@@ -25,12 +26,12 @@ BH1750 lightMeter(0x23);
 StaticJsonDocument<200> doc;
 
 //YL-83 precipitation sensor  *****************
-int sensor_A0 = 0;    // analog pin
-int wartosc_A0;        // zmienna dla wartoĹ›ci A0
-int sensor_D0 = 3;
-int wartosc_D0;
+int RainAnalogPin = 0;    // analog pin
+int RainAnalogPinValue;        
+int RainDigitalPin = 3;    //digital pin 
+int RainDigitalPinValue;
 
-// bmp180 //ZAWSZE DO 3,3 V !!!!!!!!!!!!!!!!!!!!!!!!!!
+// bmp180 // 3,3 V always! !!!!!!!!!!!!!!!!!!!!!!!!!!
 //address 0x77
 #include <SFE_BMP180.h>
 SFE_BMP180 pressure;
@@ -43,11 +44,11 @@ double T, P;
 void setup() {
   Serial.begin(115200);
   delay(2000);
-  check_dht22_status();
-  Serial.println("11111 ");
+  //check_dht22_status();
+  //Serial.println("11111 ");
 
   bh1750_initialization();
-  pinMode(sensor_D0, INPUT);    // for  precipitation sensor
+  pinMode(RainDigitalPin, INPUT);    // for  precipitation sensor
 
   bmp180_initialization();
 
@@ -58,54 +59,98 @@ void setup() {
 
 void bmp180_initialization()
 {
-  if (pressure.begin())
-    Serial.println("BMP180 init success");
-  else
-  {
-    Serial.println("BMP180 init fail\n\n");
-    while (1);
-  }
+  pressure.begin();
+//  if (pressure.begin())
+//    Serial.println("BMP180 init success");
+//  else
+//  {
+//    Serial.println("BMP180 init fail\n\n");
+//    while (1);
+//  }
 }
 
 
 void check_dht22_status()
 {
   int check_status = DHT22.read(DHT22PIN);
-  Serial.print("Stan sensora: ");
+  Serial.print("Sensor status : ");
   switch (check_status)
   {
     case DHTLIB_OK:
       Serial.print("OK\t");
       break;
     case DHTLIB_ERROR_CHECKSUM:
-      Serial.println("Błąd sumy kontrolnej");
+      Serial.println("Checksum error");
       break;
     case DHTLIB_ERROR_TIMEOUT:
-      Serial.println("Koniec czasu oczekiwania - brak odpowiedzi");
+      Serial.println("End of waiting time - no answer");
       break;
     default:
-      Serial.println("Nieznany błąd");
+      Serial.println("Unknown error");
       break;
   }
 }
 
 void bh1750_initialization() {
   //BH1750 light sensor*******************************************
-  Serial.println("11112 ");
+  //Serial.println("11112 ");
   Wire.begin();
   // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
-  Serial.println("11113 ");
+  //Serial.println("11113 ");
   if (lightMeter.begin()) {
-    Serial.println("BH1750 initialised");
+    //Serial.println("BH1750 initialised");
   }
   else {
-    Serial.println("Error initialising BH1750");
+    //Serial.println("Error initialising BH1750");
   }
-  Serial.println("11114 ");
+  //Serial.println("11114 ");
+}
+
+int getPresseure()
+{
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        delay(status);
+
+        status = pressure.getPressure(P, T);
+        if (status != 0)
+        {
+          return(P);
+        }         
+      }
+    }
+  }
+ 
+  
+}
+
+String rainFunction(int value)
+{
+  if (value > 900)
+  {
+    return "NO_RAIN";  
+  }
+  else if (value < 900 && value > 600){
+    return "SMALL_RAIN";  
+  }else if (value < 600 && value > 400){
+    return "MEDIUM_RAIN";  
+  }else if (value < 400){
+    return "STRONG_RAIN";  
+  }
+  return "ERROR";
 }
 
 
-void printinDebugMode()
+void printInDebugMode()
 {
   lap_number++;
   Serial.println();
@@ -114,9 +159,9 @@ void printinDebugMode()
 
   //DHT22 data :
   DHT22.read(DHT22PIN);
-  Serial.print("Wilgotnosc (%): ");              //wyświetlenie wartości wilgotności
+  Serial.print("Wilgotnosc (%): ");             
   Serial.println((float)DHT22.humidity, 2);
-  Serial.print("Temperatura (C): ");           //wyświetlenie temperatury
+  Serial.print("Temperatura (C): ");           
   Serial.println((float)DHT22.temperature, 2);
 
   // BH 1750 data
@@ -126,12 +171,12 @@ void printinDebugMode()
   Serial.println(" lx");
 
   //Rain sensor data :
-  wartosc_A0 = analogRead(sensor_A0);      // pobranie wartosci z A0
+  RainAnalogPinValue = analogRead(RainAnalogPin);      
   Serial.print("A0: ");
-  Serial.println(wartosc_A0);
-  wartosc_D0 = digitalRead(sensor_D0);     // pobranie wartosci z D0
-  Serial.print("D0: ");                    // wyswietlenie na monitorze szeregowym
-  Serial.println(wartosc_D0);
+  Serial.println(RainAnalogPinValue);
+  RainDigitalPinValue = digitalRead(RainDigitalPin);     
+  Serial.print("D0: ");                    
+  Serial.println(RainDigitalPinValue);
 
   //bmp180
   // Print out the measurement:
@@ -189,13 +234,19 @@ void printInJson()
   doc["Temperature"] = DHT22.temperature;  
 
   // BH 1750 data
-  uint16_t lux = lightMeter.readLightLevel();
-  String lightString = (String)lux;
-  lightString += "lx"; 
-  doc["Light"] = lightString;
+  //uint16_t lux = lightMeter.readLightLevel();
+  int lux = lightMeter.readLightLevel();
+  doc["Light"] = lux; 
+  //String lightString = (String)lux;
+  //lightString += "lx"; 
+  //doc["Light"] = lightString;
 
+  //Rain sensor data :
+  doc["Rain"] = rainFunction(analogRead(RainAnalogPin));
 
-  Serial.println();
+  doc["Pressure"] = getPresseure();  
+  
+  //Serial.println();
   serializeJsonPretty(doc, Serial);
   delay(4000);
 }
@@ -206,7 +257,7 @@ void printInJson()
 void loop() {
 
   printInJson();
-  //printinDebugMode(); 
+  //printInDebugMode(); 
 }
 //*******************************************
 //***************LOOP END********************
